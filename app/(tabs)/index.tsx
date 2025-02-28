4adcbd359c2ec4bafe70d4a02cfa7fa604780291
ShikaroX <hugo68543@gmail.com>
2025-02-28 09:51:18 +0000

    First commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,62 @@
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bem-vindo à CarApp!</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => router.push('/screens/addScreen')}>
+          <Text style={styles.buttonText}>Adicionar veículo</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#000',
+    paddingTop: 120, 
+  },
+
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    marginBottom: 20,
+  },
+
+  buttonContainer: {
+    flex: 1, // 
+    justifyContent: 'flex-end',
+    marginBottom: 600, 
+  },
+
+  button: {
+    backgroundColor: '#3498db',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
---
+++ app/screens/addScreen.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Alert, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+
+export default function AddScreen() {
+    console.log('AddScreen Loaded');
+    const router = useRouter();
+
+    const [matricula, setMatricula] = useState('');
+    const [marca, setMarca] = useState('');
+    const [modelo, setModelo] = useState('');
+    const [ano, setAno] = useState('');
+    const [cor, setCor] = useState('');
+    const [quilometros, setQuilometros] = useState('');
+
+    const handleCarData = async () => {
+        if (!matricula || !marca || !modelo || !ano || !cor || !quilometros) {
+            Alert.alert('Erro', 'Todos os campos são obrigatórios!');
+            return;
+        }
+
+        const newCar = {
+            marca,
+            modelo,
+            ano: parseInt(ano),
+            cor,
+            quilometros: parseInt(quilometros),
+        };
+
+        try {
+            const storedCars = await AsyncStorage.getItem('carros');
+            let carros = storedCars ? JSON.parse(storedCars) : {};
+
+            carros[matricula] = newCar;
+
+            await AsyncStorage.setItem('carros', JSON.stringify(carros));
+
+            Alert.alert('Sucesso', 'Veículo adicionado com sucesso!');
+            console.log('Carro guardado:', newCar);
+
+            setMatricula('');
+            setMarca('');
+            setModelo('');
+            setAno('');
+            setCor('');
+            setQuilometros('');
+        } catch (error) {
+            console.log('Erro ao guardar veículo:', error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Adicionar veículo</Text>
+
+            <TextInput style={styles.input} placeholder='Matrícula (XX-XX-XX)' placeholderTextColor='#cccccc' value={matricula} onChangeText={setMatricula} />
+            <TextInput style={styles.input} placeholder='Marca' placeholderTextColor='#cccccc' value={marca} onChangeText={setMarca} />
+            <TextInput style={styles.input} placeholder='Modelo' placeholderTextColor='#cccccc' value={modelo} onChangeText={setModelo} />
+            <TextInput style={styles.input} placeholder='Ano' placeholderTextColor='#cccccc' keyboardType="numeric" value={ano} onChangeText={setAno} />
+            <TextInput style={styles.input} placeholder='Cor' placeholderTextColor='#cccccc' value={cor} onChangeText={setCor} />
+            <TextInput style={styles.input} placeholder='Quilómetros' placeholderTextColor='#cccccc' keyboardType="numeric" value={quilometros} onChangeText={setQuilometros} />
+
+            <TouchableOpacity style={styles.buttonAdd} onPress={handleCarData}>
+                <Text style={styles.buttonText}>Adicionar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonBack} onPress={() => router.push('/')}>
+                <Text style={styles.buttonText}>Voltar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#000',
+    },
+    title: {
+        color: 'white',
+        fontSize: 30,
+        textAlign: 'center',
+        marginBottom: 50,
+        marginTop: 30,
+    },
+    input: {
+        backgroundColor: '#3498db',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+        width: '80%',
+        color: 'white',
+    },
+    buttonAdd: {
+        backgroundColor: '#4469cf',
+        padding: 16,
+        width: '50%',
+        marginTop: 30,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonBack: {
+        backgroundColor: '#570d0d',
+        padding: 16,
+        width: '50%',
+        marginTop: 30,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
